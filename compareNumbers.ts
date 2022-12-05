7ca9e44083baa1478e7d960952cea5a16ddd7675
Sarah Haswell <shaswell@scottlogic.com>
2022-12-05 16:00:35 +0000

    Day 1 plus number compare util

---
+++ compareNumbers.ts
@@ -0,0 +1,10 @@
+export function compareNumbers(a: Number, b: Number) {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    // a must be equal to b
+    return 0;
+}
---
+++ day1.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+import { compareNumbers } from "./compareNumbers";
+
+const getHighestLoad = (input: string) => {
+    const splitByElf = input.split('\n\n');
+    const numElves = splitByElf.length;
+
+    const added  = splitByElf.map((elfLoad: string) =>
+        elfLoad.split('\n')
+            .map((numberString: string) => parseInt(numberString.trim()))
+            .filter(x => !Number.isNaN(x))
+            .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    )
+    .sort(compareNumbers);
+
+    return added[numElves-1];
+}
+
+const inputFile = readFileSync("inputDay1.txt");
+console.log("Answer is: " + getHighestLoad(inputFile.toString()));
