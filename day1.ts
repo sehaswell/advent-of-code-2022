8c2828074e1de0ed8382cba4c61c9a03ba61b53e
Sarah Haswell <shaswell@scottlogic.com>
2022-12-05 16:14:13 +0000

    Add day1 part 2

--- day1.ts
+++ day1.ts
@@ -1,20 +1,29 @@
 import { readFileSync } from "fs";
 import { compareNumbers } from "./utils/compareNumbers";
 
-const getHighestLoad = (input: string) => {
-    const splitByElf = input.split('\n\n');
-    const numElves = splitByElf.length;
+const getSortedLoads = (input: string): number[] => {
+    return input.split('\n\n')
+        .map((elfLoad: string) =>
+            elfLoad.split('\n')
+                .map((numberString: string) => parseInt(numberString.trim()))
+                .filter(x => !Number.isNaN(x))
+                .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+        )
+        .sort(compareNumbers);
+}
 
-    const added  = splitByElf.map((elfLoad: string) =>
-        elfLoad.split('\n')
-            .map((numberString: string) => parseInt(numberString.trim()))
-            .filter(x => !Number.isNaN(x))
-            .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-    )
-    .sort(compareNumbers);
+const getHighestLoad = (sortedLoads: number[]) => {
+    return sortedLoads[sortedLoads.length - 1];
+}
 
-    return added[numElves-1];
+const getTopThreeSum = (sortedLoads: number[]) => {
+    const lastThree = sortedLoads.splice(sortedLoads.length - 3, sortedLoads.length - 1);
+    return lastThree.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
 const inputFile = readFileSync("inputDay1.txt");
-console.log("Answer is: " + getHighestLoad(inputFile.toString()));
+const sortedLoads = getSortedLoads(inputFile.toString());
+
+console.log("Answer is: " + getHighestLoad(sortedLoads));
+
+console.log("Part two answer is: " + getTopThreeSum(sortedLoads));
